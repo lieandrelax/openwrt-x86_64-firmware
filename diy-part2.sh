256ab23f27359af7bbf6277bd65df505261e7e82
lieandrelax <161298604+lieandrelax@users.noreply.github.com>
2024-08-07 16:36:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,5 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/lieandrelax/openwrt-23.05.3 package/hello_world
